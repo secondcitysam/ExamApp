c8cf288e3fdc4431b6f437310f0570b38f102c69
secondcitysam <samyak.kedari4444@gmail.com>
2025-11-10 12:23:00 +0530

    Added Render deployment confhg

--- app.py
+++ app.py
@@ -39,10 +39,21 @@ warnings.filterwarnings("ignore")
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.secret_key = 'xyz'
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'kotlin2025'
-app.config['MYSQL_DB'] = 'examproctordb'
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'kotlin2025'
+# app.config['MYSQL_DB'] = 'examproctordb'
+
+
+import os
+
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', 3306))  # default if missing
+
+
 mysql = MySQL(app)
 
 executor = ThreadPoolExecutor(max_workers=4)
